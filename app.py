c2957d7126bcd54977517a8c35187240858aafe4
Daniela Bosch <danielarbosch@gmail.com>
2025-10-27 20:59:53 -0300

    Read buttons and keyboard independently

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import argparse
 
+from queue import Queue
+
 from src.serials.escpos_printer import Printer
+from src.serials.buttons import Buttons
+from src.serials.keyboard import Keyboard
 from src.modes.directory import DirectoryMode
 from src.modes.photo import PhotoMode
 from src.wrappers.base import __all__ as wrappers
 
 
 if __name__ == "__main__":
-    print("Iniciando conexión con la impresora...")
+    print("***************************************")
+    print("*       |WW|                          *")
+    print("*      _|  |_       Tickepolaroid     *")
+    print("*     ( |__| )                        *")
+    print("*    /________\   Imágenes impresas   *")
+    print("*   |       o  |      en 1-bit        *")
+    print("*   |__________|    por @danipupy     *")
+    print("*                                     *")
+    print("***************************************")
 
     parser = argparse.ArgumentParser(add_help=True)
 
@@ -29,21 +41,31 @@ if __name__ == "__main__":
         """,
     )
     args = parser.parse_args()
-    
-    if args.help:
-        parser.print_help()
-        raise SystemExit(0)
-    
+
+    input_queue = Queue()
+
     printer = Printer()
     printer.reset()
+    buttons = Buttons(input_queue)
+    keyboard = Keyboard(input_queue)
+    buttons.start()
+    keyboard.start()
 
-    if args.modo == "archivos":
-        dm = DirectoryMode(printer, args.evento)
-        dm.run()
-    elif args.modo == "foto":
-        pm = PhotoMode(printer, args.evento)
-        pm.run()
-    else:
-        print("Modo desconocido, elija entre las opciones: archivos, foto")
-
-    printer.close()
+    try:
+        if args.modo == "archivos":
+            dm = DirectoryMode(printer, args.evento, input_queue)
+            dm.run()
+        elif args.modo == "foto":
+            pm = PhotoMode(printer, args.evento, input_queue)
+            pm.run()
+        else:
+            print("Modo desconocido, elija entre las opciones: archivos, foto")
+    except KeyboardInterrupt:
+        print("Interrupción del usuario")
+    finally:
+        print("Finalizando conexión con la impresora...")
+        printer.close()
+        print("Finalizando conexión con la botonera...")
+        buttons.join(timeout=1)
+        print("Finalizando conexión con el teclado...")
+        keyboard.join(timeout=1)
