73ac7dc3a126e4fc9068874d144fe7e91194bf91
Daniela Bosch <danielarbosch@gmail.com>
2026-04-23 16:35:12 -0300

    VCF 2026 wrapper

---
+++ src/wrappers/vcf_2026.py
@@ -0,0 +1,6 @@
+class VCF2026:
+    def print_pre(self, printer):
+        printer.write_text("Vintage Computer Festival 2026")
+
+    def print_post(self, printer):
+        printer.write_text("@danipupy@mastodon.art")
